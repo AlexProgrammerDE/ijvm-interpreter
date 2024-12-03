161892bdb997e95bb4aa3c32ae9521837da03f96
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-12-03 14:54:23 +0100

    Import gradle

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+testing {
+    suites {
+        @Suppress("UnstableApiUsage")
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter("5.10.3")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
